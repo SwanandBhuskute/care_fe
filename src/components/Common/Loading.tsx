ef84620aa0083d162c72ef8de4beaaeae05e979c
SwanandBhuskute <swanandbhuskute2003@gmail.com>
2024-11-11 14:28:29 +0000

    repositioned the loader at center of page for routes

--- src/components/Common/Loading.tsx
+++ src/components/Common/Loading.tsx
@@ -2,13 +2,11 @@ const img = "/images/care_logo_gray.svg";
 
 const Loading = () => {
   return (
-    <div className="grid">
-      <div className="col-span-12 flex items-center justify-center sm:col-span-12 md:col-span-12 lg:col-span-12">
-        <div className="App">
-          <header className="App-header">
-            <img src={img} className="App-logo" alt="logo" />
-          </header>
-        </div>
+    <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center z-50">
+      <div className="App">
+        <header className="App-header">
+          <img src={img} className="App-logo" alt="logo" />
+        </header>
       </div>
     </div>
   );
