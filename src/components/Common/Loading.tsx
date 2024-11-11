23ef4cf826c80488b74d9eea05a34eee33aaa4e4
SwanandBhuskute <swanandbhuskute2003@gmail.com>
2024-11-11 15:47:30 +0000

    added status to the element and changed alt

--- src/components/Common/Loading.tsx
+++ src/components/Common/Loading.tsx
@@ -4,8 +4,8 @@ const Loading = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center z-50">
       <div className="App">
-        <header className="App-header">
-          <img src={img} className="App-logo" alt="logo" />
+        <header className="App-header" role="status" aria-live="polite">
+          <img src={img} className="App-logo" alt="Loading..." />
         </header>
       </div>
     </div>
