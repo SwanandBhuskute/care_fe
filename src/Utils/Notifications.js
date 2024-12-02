6ebc815f707b85de50386935f5c7fe186f47987a
SwanandBhuskute <swanandbhuskute2003@gmail.com>
2024-12-02 19:14:30 +0000

    fixing

--- src/Utils/Notifications.js
+++ src/Utils/Notifications.js
@@ -1,7 +1,7 @@
 import { Stack, alert, defaultModules } from "@pnotify/core";
 import * as PNotifyMobile from "@pnotify/mobile";
 
-import { camelCase, cleanString } from "@/Utils/utils";
+import { capitalizeWords, cleanString } from "@/Utils/utils";
 
 defaultModules.set(PNotifyMobile, {});
 
@@ -45,7 +45,7 @@ const notifyError = (error) => {
     errorMsg = error.detail;
   } else {
     for (let [key, value] of Object.entries(error)) {
-      let keyName = cleanString(camelCase(key));
+      let keyName = capitalizeWords(cleanString(key));
       if (Array.isArray(value)) {
         const uniques = [...new Set(value)];
         errorMsg += `${keyName} - ${uniques.splice(0, 5).join(", ")}`;
